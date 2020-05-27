df501a1c3eab90e268e9c24685d176e7913612e7
Mayank Kumar <makumar@infoblox.com>
2020-05-27 17:14:31 +0530

    mqtt pub and sub client

--- pubsub/mqtt/mqtt.go
+++ pubsub/mqtt/mqtt.go
@@ -36,11 +36,17 @@ const (
 	defaultCleanSession = true
 )
 
+var (
+	clientIDSub = "%s-sub"
+	clientIDPub = "%s-pub"
+)
+
 // mqttPubSub type allows sending and receiving data to/from MQTT broker.
 type mqttPubSub struct {
-	client   mqtt.Client
-	metadata *metadata
-	logger   logger.Logger
+	clientSub mqtt.Client
+	clientPub mqtt.Client
+	metadata  *metadata
+	logger    logger.Logger
 }
 
 // NewMQTTPubSub returns a new mqttPubSub instance.
@@ -101,17 +107,15 @@ func (m *mqttPubSub) Init(metadata pubsub.Metadata) error {
 		return err
 	}
 	m.metadata = mqttMeta
+	clientIDSub = fmt.Sprintf(clientIDSub, m.metadata.clientID)
+	clientIDPub = fmt.Sprintf(clientIDPub, m.metadata.clientID)
 
-	uri, err := url.Parse(m.metadata.url)
-	if err != nil {
-		return err
-	}
-	client, err := m.connect(uri)
+	clientP, err := m.connect(clientIDPub)
 	if err != nil {
 		return err
 	}
 
-	m.client = client
+	m.clientPub = clientP
 
 	m.logger.Debug("mqtt message bus initialization complete")
 	return nil
@@ -121,29 +125,64 @@ func (m *mqttPubSub) Init(metadata pubsub.Metadata) error {
 func (m *mqttPubSub) Publish(req *pubsub.PublishRequest) error {
 	m.logger.Debugf("mqtt publishing topic %s with data: %v", req.Topic, req.Data)
 
-	token := m.client.Publish(req.Topic, m.metadata.qos, m.metadata.retain, req.Data)
+	token := m.clientPub.Publish(req.Topic, m.metadata.qos, m.metadata.retain, req.Data)
 	if !token.WaitTimeout(defaultWait) || token.Error() != nil {
 		return fmt.Errorf("mqtt error from publish: %v", token.Error())
 	}
 	return nil
 }
 
-// Subscribe to the mqtt pub sub topic.
-func (m *mqttPubSub) Subscribe(req pubsub.SubscribeRequest, handler func(msg *pubsub.NewMessage) error) error {
-	token := m.client.Subscribe(
-		req.Topic,
-		m.metadata.qos,
-		func(client mqtt.Client, mqttMsg mqtt.Message) {
-			handler(&pubsub.NewMessage{Topic: req.Topic, Data: mqttMsg.Payload()})
-		})
+func (m *mqttPubSub) subscribeHandler(req pubsub.SubscribeRequest, handler func(msg *pubsub.NewMessage) error) error {
+	registerHandler := func(client mqtt.Client, mqttMsg mqtt.Message) {
+		msg := &pubsub.NewMessage{Topic: mqttMsg.Topic(), Data: mqttMsg.Payload()}
+		if err := handler(msg); err != nil {
+			m.clientSub.Disconnect(0)
+		}
+	}
+	token := m.clientSub.Subscribe(req.Topic, m.metadata.qos, registerHandler)
 	if err := token.Error(); err != nil {
-		return fmt.Errorf("mqtt error from subscribe: %v", err)
+		return err
 	}
 	return nil
 }
 
-func (m *mqttPubSub) connect(uri *url.URL) (mqtt.Client, error) {
-	opts := m.createClientOptions(uri)
+// Subscribe to the mqtt pub sub topic.
+func (m *mqttPubSub) Subscribe(req pubsub.SubscribeRequest, handler func(msg *pubsub.NewMessage) error) error {
+	// initial subscription
+	var err error
+	m.clientSub, err = m.connect(clientIDSub)
+	if err != nil {
+		return err
+	}
+	err = m.subscribeHandler(req, handler)
+	if err != nil {
+		return fmt.Errorf("mqtt error from subscribe: %v", err)
+	}
+
+	go func(mps *mqttPubSub) {
+		timer := time.NewTicker(defaultWait)
+		defer timer.Stop()
+		for {
+			select {
+			case <-timer.C:
+				if !mps.clientSub.IsConnected() {
+					mps.logger.Debug("connecting for subscription")
+					mps.clientSub, _ = mps.connect(clientIDSub)
+					mps.subscribeHandler(req, handler)
+				}
+			}
+		}
+	}(m)
+
+	return nil
+}
+
+func (m *mqttPubSub) connect(clientID string) (mqtt.Client, error) {
+	uri, err := url.Parse(m.metadata.url)
+	if err != nil {
+		return nil, err
+	}
+	opts := m.createClientOptions(uri, clientID)
 	client := mqtt.NewClient(opts)
 	token := client.Connect()
 	for !token.WaitTimeout(defaultWait) {
@@ -154,9 +193,9 @@ func (m *mqttPubSub) connect(uri *url.URL) (mqtt.Client, error) {
 	return client, nil
 }
 
-func (m *mqttPubSub) createClientOptions(uri *url.URL) *mqtt.ClientOptions {
+func (m *mqttPubSub) createClientOptions(uri *url.URL, clientID string) *mqtt.ClientOptions {
 	opts := mqtt.NewClientOptions()
-	opts.SetClientID(m.metadata.clientID)
+	opts.SetClientID(clientID)
 	opts.SetCleanSession(m.metadata.cleanSession)
 	opts.AddBroker(uri.Host)
 	opts.SetUsername(uri.User.Username())
